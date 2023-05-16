0b0b7e920714e39f113fdad591c1f4951158d1fc
Carl Bodin <bodincarl@gmail.com>
2023-05-16 20:31:32 +0200

    First draft simple calculator

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
 class CalcApp(App):
@@ -8,16 +9,63 @@ class CalcApp(App):
 
 
 class MainWidget(Widget):
+    display = ObjectProperty(None)
+    operators = ['+', '-', '*', '/']
 
     def evaluate_expression(self, expression):
+        
+        # Handle empty evaluations
+        if expression == "":
+            return ""      
+        
+        # Exception handling
+        try:
+            result = str(eval(expression))
+        except ZeroDivisionError:
+            return 'Never divide by zero...'
+        except SyntaxError:
+            return 'Syntax Error'
+        except:
+            return 'ERROR'
 
-        result = str(eval(expression))
-
+        # Convert float to int when x.0
         split_str = result.split('.')
-        if split_str[1] == '0':
+        if split_str[-1] == '0':
             result = split_str[0]
         
         return result
+    
+    @staticmethod
+    def easter_eggs(expression):
+
+        if expression == "1337":
+            return "ELITE"
+        
+        elif expression == "80085":
+            return "Rly?"
+        
+        elif expression in ["01134", "14"]:
+            return "Hey! How are you?"
+        
+        elif expression == "1+1":
+            return "You should know this one"
+        
+        elif expression == "You should know this one":
+            return "3 :P"
+
+    def handle_operators(self, operator):
+
+        if self.display.text == '':
+            self.display.text = operator
+            return
+
+        # Check for bad expression
+        expression = self.display.text
+        if expression[-1] in self.operators:
+            expression = expression[:-1]
+        
+        self.display.text = expression + operator
+        
 
 if __name__ == '__main__':
     CalcApp().run()
